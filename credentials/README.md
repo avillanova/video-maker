54b29122fa73e6b277b9bb42160c80f3ea79f952
unknown <alexsrvillanova@gmail.com>
2019-03-18 15:58:45 -0300

    Watson

--- credentials/README.md
+++ credentials/README.md
@@ -9,3 +9,17 @@ File: `algorithmia.json`
   "apiKey": "sim8As72h56j3mBYqEOSHfLE4k81"
 }
 ```
+
+## Watson-nlu
+File: `watson-nlu.json`  
+
+```
+{
+  "apikey": "vOFV4GcaCVDFuTh6fRwP91IdSTEd2362ZU59QkteNo9z",
+  "iam_apikey_description": "Auto generated apikey during resource-key operation for Instance - crn:v1:bluemix:public:natural-language-understanding:us-south:a/4b863888ad4e441787c25b32d725ea83:b3f4441f-e72c-4bca-b1bb-69675943c469::",
+  "iam_apikey_name": "auto-generated-apikey-4798b4d3-5576-4074-be1f-aef069d54847",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/4b863888ad4e441787c25b32d725ea83::serviceid:ServiceId-705e0231-273c-4142-888f-4369b3666f40",
+  "url": "https://gateway.watsonplatform.net/natural-language-understanding/api"
+}
+```
--- index.js
+++ index.js
@@ -1,47 +1,31 @@
 const readline = require('readline-sync')
-const Parser = require('rss-parser')
 const robots = {
   text: require('./robots/text.js')
 }
 
+async function start() {
+  const content = {
+    maximumSentences: 7
+  }
 
-const TREND_URL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR' 
-
-async function start () {
-  const content = {}
-
-  content.searchTerm = await askAndReturnSearchTerm()
+  content.searchTerm = askAndReturnSearchTerm()
   content.prefix = askAndReturnPrefix()
 
-  await robots.text(content);
+  await robots.text(content)
 
-
-  async function askAndReturnSearchTerm () {
-    const response = readline.question('Type a Wikipedia search term or G to fetch google trends: ')
-    return (response.toUpperCase() === 'G') ?  await askAndReturnTrend() : response     
+  function askAndReturnSearchTerm() {
+    return readline.question('Type a Wikipedia search term: ')
   }
 
-  async function askAndReturnTrend() {
-    console.log('Please Wait...')
-    const trends = await getGoogleTrends()
-    const choice = readline.keyInSelect(trends, 'Choose your trend:')
-    return trends[choice] 
-  }
-
-  async function getGoogleTrends () {
-    const parser = new Parser();
-    const trends = await parser.parseURL(TREND_URL)
-    return trends.items.map(i => i.title)
-  }
-
-  function askAndReturnPrefix () {
+  function askAndReturnPrefix() {
     const prefixes = ['Who is', 'What is', 'The history of']
     const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
     const selectedPrefixText = prefixes[selectedPrefixIndex]
+
     return selectedPrefixText
   }
 
-  console.log(content)
+  console.log(JSON.stringify(content, null, 4))
 }
 
-start()
+start();
